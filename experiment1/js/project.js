56dc88a8bc0e7d9b2d09a56a05fddc7c72514519
Quinlan <qbhoang@ucsc.edu>
2024-04-05 15:50:30 -0700

    exp 1

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -24,10 +24,53 @@ class MyProjectClass {
 
 function main() {
   // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  const fillers = {
+    person: ["indvidual", "specimen", "human-being", "god", "goddess", "spirit"],
+    compliment: ["fine", "well-crafted", "handsome", "beautiful", "hot", "gorgeous", "good-looking", "mesmerizing", "stunning", "jaw-dropping"],
+    greeting: ["Hello", "Greetings", "Good afternoon", "Good morning", "Good evening", "Pardon my interruption", "Salutations", "What's poppin", "What's up" ],
+    presence: ["presence", "stature", "sweet aroma", "glistening figure"],
+    desc: ["salvation", "endurance", "love", "youth", "wealth", "integrity", "prospect", "heart-felt memories", "divinity"],
+    reaction: ["enjoy", "rellish", "admire", "appreciate", "love", "welcome", "remember", "like"],
+    num: ["two", "a painstakinly large amount of", "thirty", "three hundred thousand and six hundred and forty nine"],
+    adj: ["heart-felt", "divine", "fantastic", "amazing", "splendid", "enjoyable", "exquisite", "adventureful", "jam-packed", "stupendous" ],
+    
+  };
+  
+  const template = `$greeting my $compliment $person!!
+  
+  I was very aware of your $presence and couldn't help but come see you. I come bearing gifts of $desc that I'm sure you will $reaction!!
+  Now I will be off on my way. I have a meeting to catch in $num minutes. I'm so glad I got meet you and have a $adj rest of your day!!
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
